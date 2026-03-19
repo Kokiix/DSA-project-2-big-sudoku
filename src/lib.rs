a3ed2a9cb29dadbfc448f3d108f3e468bf87d7fc
Kenneth <kokiidoki@gmail.com>
2026-03-19 17:57:38 -0400

    reduce overengineering, remove square/matrix type

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod tests;
 
 use wasm_bindgen::prelude::wasm_bindgen;
 
-use crate::solve_matrix::{SolvingMatrix, SquareNumber};
+use crate::solve_matrix::Node;
 
 // TODO: implement methods here so that JS doesn't have to extract?
 #[wasm_bindgen]
@@ -15,13 +15,86 @@ pub struct FinalSudokuBoard {
 }
 
 #[wasm_bindgen]
-pub fn generate_sudoku(input_sudoku_size: usize) -> Option<FinalSudokuBoard> {
-    // TODO: choose max size allowed
-    let valid_sudoku_size: SquareNumber = input_sudoku_size.try_into().ok()?;
-    let mut matrix = SolvingMatrix::new(valid_sudoku_size);
-    // SOLVE HERE
+pub fn generate_sudoku(n: usize) -> Option<FinalSudokuBoard> {
+    // TODO: set/enforce max size?
+    if n.isqrt().pow(2) != n {
+        return None;
+    }
+    let mut matrix = init_solution_space(n);
+
+    let n2 = n.pow(2);
+    let root = 4 * n2 as usize;
+    while (matrix[root].right) {}
+
     Some(FinalSudokuBoard {
         init_grid: vec![1; 81],
         solved_grid: vec![0; 81],
     })
+}
+
+fn init_solution_space(n: u32) -> Vec<Node> {
+    // rows = (all the ways the n values fit into n ^2 grid) = n^3, + 1 for headers
+    // cols = 4 constraint types, existence, row, col, subgrid, each w n^2 possible values, but each row can only match 4 total constraints
+    let mut matrix = Vec::with_capacity(((n.pow(3) + 1) * 4) as usize);
+    let n2 = n.pow(2);
+
+    /* the first 4n^2 + 1 indices will be the headers, with the constraint subsections ordered as follows:
+    section = starting index
+    existence = 0
+    row = n^2
+    col = 2n^2
+    subgrid = 3n^2
+    root = 4n^2
+
+    within each subsection, entries go from 1 to n in the first position, then the 2nd, etc
+    */
+    let root_idx = 4 * n2;
+    // first col obj, linked to root
+    matrix.push(Node {
+        column_obj: 0,
+        up: 0,
+        down: 0,
+        right: 1,
+        left: root_idx,
+        column_size: Some(0),
+    });
+    for col_idx in 1..root_idx {
+        matrix.push(Node {
+            column_obj: col_idx,
+            up: col_idx,
+            down: col_idx,
+            right: col_idx + 1,
+            left: col_idx - 1,
+            column_size: Some(0),
+        });
+    }
+    // root col obj
+    matrix.push(Node {
+        column_obj: root_idx,
+        up: root_idx,
+        down: root_idx,
+        right: 0,
+        left: root_idx - 1,
+        column_size: Some(0),
+    });
+
+    // initialize matrix nodes
+    let mut new_node_pos = root_idx + 1;
+    for grid_value in 0..n {
+        for grid_position in 0..n2 {
+            Node::insert_row(
+                &mut matrix,
+                new_node_pos,
+                &[
+                    // pointers to the column objects corresponding to each item in the row
+                    grid_position,                           // existence
+                    n2 + grid_position / n + grid_value,     // row
+                    2 * n2 + grid_position % n + grid_value, // col
+                    3 * n2 + grid_position / n + grid_value, // subgrid
+                ],
+            );
+            new_node_pos += 4;
+        }
+    }
+    return matrix;
 }
--- src/solve_matrix.rs
+++ src/solve_matrix.rs
@@ -1,12 +1,12 @@
 // TODO: prove these statements with math
 // u16 is too small; max square sudoku size is ~144
-struct Node {
-    column_obj: u32,
-    up: u32,
-    down: u32,
-    right: u32,
-    left: u32,
-    column_size: Option<u32>,
+pub struct Node {
+    pub column_obj: u32,
+    pub up: u32,
+    pub down: u32,
+    pub right: u32,
+    pub left: u32,
+    pub column_size: Option<u32>,
 }
 
 impl Node {
@@ -32,100 +32,5 @@ impl Node {
 
         matrix[starting_pos as usize].left = starting_pos + 3;
         matrix[starting_pos as usize + 3].right = starting_pos;
-    }
-}
-
-pub struct SolvingMatrix {
-    sudoku_size: usize,
-    matrix: Vec<Node>,
-}
-
-impl SolvingMatrix {
-    pub fn new(size: SquareNumber) -> Self {
-        SolvingMatrix {
-            sudoku_size: size.value,
-            matrix: SolvingMatrix::init_solution_space(size.value as u32),
-        }
-    }
-
-    fn init_solution_space(n: u32) -> Vec<Node> {
-        // rows = (all the ways the n values fit into n ^2 grid) = n^3, + 1 for headers
-        // cols = 4 constraint types, existence, row, col, subgrid, each w n^2 possible values, but each row can only match 4 total constraints
-        let mut matrix = Vec::with_capacity(((n.pow(3) + 1) * 4) as usize);
-        let n2 = n.pow(2);
-
-        /* the first 4n^2 + 1 indices will be the headers, with the constraint subsections ordered as follows:
-        section = starting index
-        existence = 0
-        row = n^2
-        col = 2n^2
-        subgrid = 3n^2
-        root = 4n^2
-
-        within each subsection, entries go from 1 to n in the first position, then the 2nd, etc
-        */
-        let root_idx = 4 * n2;
-        // first col obj, linked to root
-        matrix.push(Node {
-            column_obj: 0,
-            up: 0,
-            down: 0,
-            right: 1,
-            left: root_idx,
-            column_size: Some(0),
-        });
-        for col_idx in 1..root_idx {
-            matrix.push(Node {
-                column_obj: col_idx,
-                up: col_idx,
-                down: col_idx,
-                right: col_idx + 1,
-                left: col_idx - 1,
-                column_size: Some(0),
-            });
-        }
-        // root col obj
-        matrix.push(Node {
-            column_obj: root_idx,
-            up: root_idx,
-            down: root_idx,
-            right: 0,
-            left: root_idx - 1,
-            column_size: Some(0),
-        });
-
-        // initialize matrix nodes
-        let mut new_node_pos = root_idx + 1;
-        for grid_value in 0..n {
-            for grid_position in 0..n2 {
-                Node::insert_row(
-                    &mut matrix,
-                    new_node_pos,
-                    &[
-                        // pointers to the column objects corresponding to each item in the row
-                        grid_position,                           // existence
-                        n2 + grid_position / n + grid_value,     // row
-                        2 * n2 + grid_position % n + grid_value, // col
-                        3 * n2 + grid_position / n + grid_value, // subgrid
-                    ],
-                );
-                new_node_pos += 4;
-            }
-        }
-        return matrix;
-    }
-}
-
-pub struct SquareNumber {
-    value: usize,
-}
-impl TryFrom<usize> for SquareNumber {
-    type Error = ();
-    fn try_from(n: usize) -> Result<Self, Self::Error> {
-        if n.isqrt().pow(2) == n {
-            Ok(SquareNumber { value: n as usize })
-        } else {
-            Err(())
-        }
     }
 }
