d7ffcf66840df6bf75244614d0e21554f4f2083b
kenneth <kokiidoki@gmail.com>
2026-03-22 21:34:25 -0400

    all my changes from earlier are gone now for some reason.. redid them

--- src/solve_matrix.rs
+++ src/solve_matrix.rs
@@ -1,5 +1,3 @@
-use std::u32;
-
 // TODO: prove these statements with math
 // u16 is too small; max square sudoku size is ~144
 #[derive(Clone)]
@@ -10,32 +8,6 @@ pub struct Node {
     pub right: u32,
     pub left: u32,
     pub column_size: Option<u32>,
-}
-
-impl Node {
-    fn create_connected(matrix: &mut Vec<Node>, pos: u32, col_obj: u32) -> Self {
-        let bot_col_node = matrix[col_obj as usize].up;
-        matrix[bot_col_node as usize].down = pos;
-        matrix[col_obj as usize].up = pos;
-        Node {
-            column_obj: col_obj,
-            up: bot_col_node,
-            down: col_obj,
-            right: pos + 1,
-            left: pos - 1,
-            column_size: None,
-        }
-    }
-
-    pub fn insert_row(matrix: &mut Vec<Node>, starting_pos: u32, col_positions: &[u32]) {
-        for (i, col_pos) in col_positions.iter().enumerate() {
-            let n = Node::create_connected(matrix, starting_pos + i as u32, *col_pos);
-            matrix.push(n);
-        }
-
-        matrix[starting_pos as usize].left = starting_pos + 3;
-        matrix[starting_pos as usize + 3].right = starting_pos;
-    }
 }
 
 pub struct Solver {
@@ -51,6 +23,94 @@ impl Solver {
         let mut s = Self::init_grid(n);
         s.explore_min_col();
         s.solution
+    }
+
+    // n must be a square number
+    fn init_grid(n: u32) -> Self {
+        // rows = (all the ways the n values fit into n ^2 grid) = n^3, + 1 for headers
+        // cols = 4 constraint types, existence, row, col, subgrid, each w n^2 possible values, but each row can only match 4 total constraints
+        let mut matrix = Vec::with_capacity(((n.pow(3) + 1) * 4) as usize);
+        let n2 = n.pow(2);
+
+        /* the first 4n^2 + 1 indices will be the headers, with the constraint subsections ordered as follows:
+        section = starting index
+        existence = 0
+        row = n^2
+        col = 2n^2
+        subgrid = 3n^2
+        root = 4n^2
+
+        within each subsection, entries go from 1 to n in the first position, then the 2nd, etc
+        */
+        let root_idx = 4 * n2;
+        // first col obj, linked to root
+        for col_idx in 0..=root_idx {
+            matrix.push(Node {
+                column_obj: col_idx,
+                up: col_idx,
+                down: col_idx,
+                right: col_idx + 1,
+                left: col_idx - 1,
+                column_size: Some(n),
+            });
+        }
+
+        // wrap edges
+        matrix[0].left = root_idx;
+        matrix[root_idx as usize].right = 0;
+        matrix[root_idx as usize].column_size = None;
+
+        // initialize matrix nodes
+        let mut new_node_pos = root_idx + 1;
+        for grid_value in 0..n {
+            for grid_position in 0..n2 {
+                Self::insert_row(
+                    &mut matrix,
+                    new_node_pos,
+                    &[
+                        // pointers to the column objects corresponding to each item in the row
+                        grid_position,                           // existence
+                        n2 + grid_position / n + grid_value,     // row
+                        2 * n2 + grid_position % n + grid_value, // col
+                        3 * n2 + grid_position / n + grid_value, // subgrid
+                    ],
+                );
+                new_node_pos += 4;
+            }
+        }
+
+        let solution: Vec<usize> = Vec::with_capacity(n2 as usize);
+        Solver {
+            matrix,
+            solution,
+            n,
+            n2,
+            root: root_idx as usize,
+        }
+    }
+
+    fn create_connected(matrix: &mut Vec<Node>, pos: u32, col_obj: u32) -> Node {
+        let bot_col_node = matrix[col_obj as usize].up;
+        matrix[bot_col_node as usize].down = pos;
+        matrix[col_obj as usize].up = pos;
+        Node {
+            column_obj: col_obj,
+            up: bot_col_node,
+            down: col_obj,
+            right: pos + 1,
+            left: pos - 1,
+            column_size: None,
+        }
+    }
+
+    fn insert_row(matrix: &mut Vec<Node>, starting_pos: u32, col_positions: &[u32]) {
+        for (i, col_pos) in col_positions.iter().enumerate() {
+            let n = Self::create_connected(matrix, starting_pos + i as u32, *col_pos);
+            matrix.push(n);
+        }
+
+        matrix[starting_pos as usize].left = starting_pos + 3;
+        matrix[starting_pos as usize + 3].right = starting_pos;
     }
 
     // false = fail on this recursive branch, true = matrix solved
@@ -136,68 +196,4 @@ impl Solver {
 
     fn cover_row(&mut self, start_pos: usize) {}
     fn uncover_col(&mut self, node_pos: u32) {}
-
-    // n must be a square number
-    fn init_grid(n: u32) -> Self {
-        // rows = (all the ways the n values fit into n ^2 grid) = n^3, + 1 for headers
-        // cols = 4 constraint types, existence, row, col, subgrid, each w n^2 possible values, but each row can only match 4 total constraints
-        let mut matrix = Vec::with_capacity(((n.pow(3) + 1) * 4) as usize);
-        let n2 = n.pow(2);
-
-        /* the first 4n^2 + 1 indices will be the headers, with the constraint subsections ordered as follows:
-        section = starting index
-        existence = 0
-        row = n^2
-        col = 2n^2
-        subgrid = 3n^2
-        root = 4n^2
-
-        within each subsection, entries go from 1 to n in the first position, then the 2nd, etc
-        */
-        let root_idx = 4 * n2;
-        // first col obj, linked to root
-        for col_idx in 0..=root_idx {
-            matrix.push(Node {
-                column_obj: col_idx,
-                up: col_idx,
-                down: col_idx,
-                right: col_idx + 1,
-                left: col_idx - 1,
-                column_size: Some(n),
-            });
-        }
-
-        // wrap edges
-        matrix[0].left = root_idx;
-        matrix[root_idx as usize].right = 0;
-        matrix[root_idx as usize].column_size = None;
-
-        // initialize matrix nodes
-        let mut new_node_pos = root_idx + 1;
-        for grid_value in 0..n {
-            for grid_position in 0..n2 {
-                Node::insert_row(
-                    &mut matrix,
-                    new_node_pos,
-                    &[
-                        // pointers to the column objects corresponding to each item in the row
-                        grid_position,                           // existence
-                        n2 + grid_position / n + grid_value,     // row
-                        2 * n2 + grid_position % n + grid_value, // col
-                        3 * n2 + grid_position / n + grid_value, // subgrid
-                    ],
-                );
-                new_node_pos += 4;
-            }
-        }
-
-        let solution: Vec<usize> = Vec::with_capacity(n2 as usize);
-        Solver {
-            matrix,
-            solution,
-            n,
-            n2,
-            root: root_idx as usize,
-        }
-    }
 }
