967771a271dbda3c37530dfcfd7f46faf0888aa6
kenneth <kokiidoki@gmail.com>
2026-03-28 11:36:37 -0400

    document test parameters better

--- dlx-solver/src/tests.rs
+++ dlx-solver/src/tests.rs
@@ -2,8 +2,14 @@ use crate::generate_sudoku;
 
 #[test]
 fn print_solved() {
+    // Parameters
+    // ========================================================
     let size = 36;
-    let sol = generate_sudoku(size as u32, 0.1, 1).unwrap();
+    let remove_proportion = 0.1;
+    let seed = 1;
+    // ========================================================
+
+    let sol = generate_sudoku(size as u32, remove_proportion, seed).unwrap();
     let init = sol.init_grid;
     for (i, n) in init.iter().enumerate() {
         if i % size == 0 {
