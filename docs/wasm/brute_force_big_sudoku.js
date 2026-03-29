3001b9c8f02df24cfbc3deba73bc17fa44e2c2b5
kenneth <kokiidoki@gmail.com>
2026-03-28 21:30:05 -0400

    begin to fix erased work on chase wasm export

--- docs/wasm/brute_force_big_sudoku.js
+++ docs/wasm/brute_force_big_sudoku.js
@@ -1,5 +1,19 @@
 /* @ts-self-types="./brute_force_big_sudoku.d.ts" */
 
+/**
+ * @param {number} size
+ * @param {Uint8Array} board_1d
+ * @returns {Uint8Array}
+ */
+export function solve(size, board_1d) {
+    const ptr0 = passArray8ToWasm0(board_1d, wasm.__wbindgen_malloc);
+    const len0 = WASM_VECTOR_LEN;
+    const ret = wasm.solve(size, ptr0, len0);
+    var v2 = getArrayU8FromWasm0(ret[0], ret[1]).slice();
+    wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    return v2;
+}
+
 function __wbg_get_imports() {
     const import0 = {
         __proto__: null,
@@ -19,10 +33,33 @@ function __wbg_get_imports() {
     };
 }
 
+function getArrayU8FromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return getUint8ArrayMemory0().subarray(ptr / 1, ptr / 1 + len);
+}
+
+let cachedUint8ArrayMemory0 = null;
+function getUint8ArrayMemory0() {
+    if (cachedUint8ArrayMemory0 === null || cachedUint8ArrayMemory0.byteLength === 0) {
+        cachedUint8ArrayMemory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8ArrayMemory0;
+}
+
+function passArray8ToWasm0(arg, malloc) {
+    const ptr = malloc(arg.length * 1, 1) >>> 0;
+    getUint8ArrayMemory0().set(arg, ptr / 1);
+    WASM_VECTOR_LEN = arg.length;
+    return ptr;
+}
+
+let WASM_VECTOR_LEN = 0;
+
 let wasmModule, wasm;
 function __wbg_finalize_init(instance, module) {
     wasm = instance.exports;
     wasmModule = module;
+    cachedUint8ArrayMemory0 = null;
     wasm.__wbindgen_start();
     return wasm;
 }
